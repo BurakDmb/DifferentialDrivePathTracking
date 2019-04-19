b829237bae922c16aed8265efd5813a137abd4d8
burakdmb <burakhan.demirbilek@gmail.com>
2019-04-19 03:56:54 +0300

    Updated arrive conditions

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ class Controller():
 
     def isArrived(self):
         print("Arrive check:",str(abs(self.current.x - self.goal.x)), str(abs(self.current.y - self.goal.y)))
-        if abs(self.current.x - self.goal.x) < 10.0 and abs(self.current.y - self.goal.y) < 10.0:
+        if abs(self.current.x - self.goal.x) < 10.0 or abs(self.current.y - self.goal.y) < 10.0:
             return True
         else:
             return False
